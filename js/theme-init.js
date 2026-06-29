94932782450207329e24c3d1841d26ada1a8b812
mbs7bayu-cell <mbs7bayu@gmail.com>
2026-06-29 07:42:22 +0700

    Add files via upload

--- js/theme-init.js
+++ js/theme-init.js
@@ -2,7 +2,7 @@ if (localStorage.getItem("theme") === "dark") {
     document.documentElement.classList.add("dark");
     document
         .querySelector('meta[name="theme-color"]')
-        .setAttribute("content", "#111827"); // warna dark
+        .setAttribute("content", "#121212"); // warna dark
 } else {
     document
         .querySelector('meta[name="theme-color"]')
