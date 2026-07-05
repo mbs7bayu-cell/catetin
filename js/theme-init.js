acc39e9f6ff0e529329fbec4237eb90f9804cfbe
mbs7bayu-cell <mbs7bayu@gmail.com>
2026-07-05 14:51:05 +0700

    Update theme-init.js

--- js/theme-init.js
+++ js/theme-init.js
@@ -1,10 +1,12 @@
 if (localStorage.getItem("theme") === "dark") {
     document.documentElement.classList.add("dark");
-    document
-        .querySelector('meta[name="theme-color"]')
-        .setAttribute("content", "#121212"); // warna dark
-} else {
-    document
-        .querySelector('meta[name="theme-color"]')
-        .setAttribute("content", "#f5f7fb"); // warna light
 }
+
+document
+    .querySelector('meta[name="theme-color"]')
+    .setAttribute(
+        "content",
+        localStorage.getItem("theme") === "dark"
+            ? "#121212"
+            : "#f5f7fb"
+    );
